89a17007e14ad5ae65d4a0231b3ce057cf08f334
sadelrosario <sherrie.delrosario@student.ateneo.edu>
2025-09-15 15:07:38 +0800

    added jump input and animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
-const speed = 10
+const speed = 100
+const jumpSpeed = -350
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	# Get the gravity from the project settings to be synced with RigidBody nodes.
+	var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+		
+	# add gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	
 	# get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -16,13 +23,29 @@ func _physics_process(delta):
 		# flip sprite to face left 
 		animated_sprite.flip_h = true
 		
+		# do jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jumpSpeed
+		
 	# select animation state
-	if direction == 0:
-		animated_sprite.play("idle")
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk")
 	else:
-		animated_sprite.play("walk")
-	
+		if velocity.y < 0:
+			# upward jump
+			animated_sprite.play("jump")
+		else:
+			# falling
+			animated_sprite.play("fall")
+		
 	# apply movement
 	if direction:
 		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()
 		
